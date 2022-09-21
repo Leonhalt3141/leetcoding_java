517169d968526a5d1bfc837521be1059a18e189d
Kentaro Kuwata <kuwaken0925@gmail.com>
2022-09-20 23:58:06 -0700

    Commit anyway

--- src/NextPermutation.java
+++ src/NextPermutation.java
@@ -1,2 +1,40 @@
+
+class Solution31 {
+    public void nextPermutation(int[] nums) {
+        int size = nums.length;
+        if (size <= 1) return;
+
+        int i = size - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = size - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
 public class NextPermutation {
 }
