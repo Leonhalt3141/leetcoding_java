9b6f33dc9516a327045227607b2cce769ebf4d00
Ken Kuwata <k-kuwata@geo-quantz.com>
2022-10-01 11:02:28 -0700

    Daily challenge

--- src/ValidPalindrome2.java
+++ src/ValidPalindrome2.java
@@ -1,50 +1,43 @@
 
 class Solution680 {
     public boolean validPalindrome(String s) {
-        int size = s.length();
-        if (size == 1) return false;
+        int i = 0;
+        int j = s.length() - 1;
 
-        int left = 0;
-        int right = size - 1;
-
-
-        boolean deleted = false;
-        char[] charArray = s.toCharArray();
-
-        while (left < right) {
-            if (charArray[left] != charArray[right] && !deleted) {
-                if (charArray[left + 1] == charArray[right]) {
-                    left++;
-                } else if (charArray[left] == charArray[right - 1]) {
-                    right--;
-                } else {
-                    System.out.println(1);
-                    return false;
-                }
-                deleted = true;
-            } else if (charArray[left] != charArray[right] && deleted) {
-                System.out.println(2 + " " + left + " " + right + " " + deleted);
-                return false;
-            } else {
-                left++;
-                right--;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return checkPalindrome(s, i, j - 1) || checkPalindrome(s, i + 1, j);
             }
+            i++;
+            j--;
         }
         return true;
     }
+
+    boolean checkPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
 }
 
 public class ValidPalindrome2 {
     public static void main(String[] args) {
         Solution680 solution = new Solution680();
 
-//        System.out.println(solution.validPalindrome("aba"));
-//
-//        System.out.println(solution.validPalindrome("abca"));
-//
-//        System.out.println(solution.validPalindrome("abc"));
-//
-//        System.out.println(solution.validPalindrome("deeee"));
+        System.out.println(solution.validPalindrome("aba"));
+
+        System.out.println(solution.validPalindrome("abca"));
+
+        System.out.println(solution.validPalindrome("abc"));
+
+        System.out.println(solution.validPalindrome("deeee"));
 
         System.out.println(solution.validPalindrome("aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga"));
     }
--- src/ValidSudoku.java
+++ src/ValidSudoku.java
@@ -9,77 +9,77 @@ class Solution38 {
         }
         return true;
     }
-    
+
     boolean checkRow(char[][] board, int i) {
         HashMap<Character, Integer> map = new HashMap<>();
-        
+
         for (char num : board[i]) {
             if (num != '.' && map.containsKey(num)) return false;
             map.put(num, 1);
         }
         return true;
     }
-    
+
     boolean checkColumn(char[][] board, int col) {
         HashMap<Character, Integer> map = new HashMap<>();
-        
+
         for (int i = 0; i < 9; i++) {
             char value = board[i][col];
-            
+
             if (value != '.' && map.containsKey(value)) return false;
             map.put(value, 1);
         }
         return true;
     }
-    
+
     boolean checkSubBox(char[][] board, int id) {
         int iMin, iMax, jMin, jMax = 0;
-        
+
         if (id == 0) {
-             iMin = 0;
-             iMax = 3;
-             jMin = 0;
-             jMax = 3;
+            iMin = 0;
+            iMax = 3;
+            jMin = 0;
+            jMax = 3;
         } else if (id == 1) {
-             iMin = 0;
-             iMax = 3;
-             jMin = 3;
-             jMax = 6;
+            iMin = 0;
+            iMax = 3;
+            jMin = 3;
+            jMax = 6;
         } else if (id == 2) {
-             iMin = 0;
-             iMax = 3;
-             jMin = 6;
-             jMax = 9;
+            iMin = 0;
+            iMax = 3;
+            jMin = 6;
+            jMax = 9;
         } else if (id == 3) {
-             iMin = 3;
-             iMax = 6;
-             jMin = 0;
-             jMax = 3;
+            iMin = 3;
+            iMax = 6;
+            jMin = 0;
+            jMax = 3;
         } else if (id == 4) {
-             iMin = 3;
-             iMax = 6;
-             jMin = 3;
-             jMax= 6;
+            iMin = 3;
+            iMax = 6;
+            jMin = 3;
+            jMax= 6;
         } else if (id == 5) {
-             iMin = 3;
-             iMax = 6;
-             jMin = 6;
-             jMax = 9;
+            iMin = 3;
+            iMax = 6;
+            jMin = 6;
+            jMax = 9;
         } else if (id == 6) {
-             iMin = 6;
-             iMax = 9;
-             jMin = 0;
-             jMax = 3;
+            iMin = 6;
+            iMax = 9;
+            jMin = 0;
+            jMax = 3;
         } else if (id == 7) {
-             iMin = 6;
-             iMax = 9;
-             jMin = 3;
-             jMax = 6;
+            iMin = 6;
+            iMax = 9;
+            jMin = 3;
+            jMax = 6;
         } else {
-             iMin = 6;
-             iMax = 9;
-             jMin = 6;
-             jMax = 9;
+            iMin = 6;
+            iMax = 9;
+            jMin = 6;
+            jMax = 9;
         }
 
         HashMap<Character, Integer> map = new HashMap<>();
@@ -92,7 +92,7 @@ class Solution38 {
         }
         return true;
     }
-    
+
 }
 
 public class ValidSudoku {
